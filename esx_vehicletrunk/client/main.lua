f754a1dbc0ee7543c31b43a2a555006db2a4983b
Arkansas Tom Jones <atte.kuosmanen@edu.savonia.fi>
2018-03-09 10:11:51 +0200

    Update main.lua

--- esx_vehicletrunk/client/main.lua
+++ esx_vehicletrunk/client/main.lua
@@ -19,12 +19,7 @@ function dbg(msg)
 	end
 end
 ---
-Citizen.CreateThread(function()
-  while ESX == nil do
-    TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
-    Citizen.Wait(0)
-  end
-end)
+TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
 
 RegisterNetEvent('esx:playerLoaded')
 AddEventHandler('esx:playerLoaded', function(xPlayer)
