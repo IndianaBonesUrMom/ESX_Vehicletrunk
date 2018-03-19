043adc2fe21b12424b1cb124bf86e2b571e8ec4b
MrLumi <lumilesff@hotmail.com>
2018-03-19 16:22:58 +0000

    Create en.lua

---
+++ esx_vehicletrunk/locales/en.lua
@@ -0,0 +1,8 @@
+Locales['en'] = {
+  ['add_item'] = 'The item was ~g~stored~w~.',
+  ['trunk_full'] = 'The trunk is ~w~full~r~',
+  ['saved'] = '~r~Saved~r~.',
+  ['w_quant'] = '~w~Invalid quantity~r~!',
+  ['item_nfound'] = '~r~Item not found~w~!',
+  ['error'] = '~r~Error~w~!',
+ }
