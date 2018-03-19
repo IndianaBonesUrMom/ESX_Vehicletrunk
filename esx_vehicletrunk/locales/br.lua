14cc96a15eac478866d26b8f67827987ff16a7c3
MrLumi <lumilesff@hotmail.com>
2018-03-19 17:01:41 +0000

    Update br.lua

--- esx_vehicletrunk/locales/br.lua
+++ esx_vehicletrunk/locales/br.lua
@@ -5,4 +5,12 @@ Locales['br'] = {
   ['w_quant'] = '~w~Quantidade invalida~r~!',
   ['item_nfound'] = '~r~Item nao encontrado~w~!',
   ['error'] = '~r~Erro~w~!',
+---Titulos
+  ['weap'] = 'Armas',
+  ['quantity'] = 'Quantidade',
+  ['trunk'] = 'Bagageira',
+  ['sure'] = 'Tem certeza que queres guardar uma',
+  ['surer'] = 'Tem a certeza que quer tirar a',
+  ['add_item'] = 'Lista de items',
+  ['add_weap'] = 'Lista de armas',
  }
