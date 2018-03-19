bdbd5f57199205631a1c68e1860b53ce0b9fc779
MrLumi <lumilesff@hotmail.com>
2018-03-19 17:02:15 +0000

    Update fr.lua

--- esx_vehicletrunk/locales/fr.lua
+++ esx_vehicletrunk/locales/fr.lua
@@ -1,8 +1,16 @@
 Locales['fr'] = {
-  ['add_item'] = 'l'article était ~g~stocké~w~.',
+  ['add_item'] = 'l\article etait ~g~stocke~w~.',
   ['trunk_full'] = 'Le coffre est ~w~plein~r~',
-  ['saved'] = '~r~Enregistré~r~.',
-  ['w_quant'] = '~w~Quantité invalide~r~!',
+  ['saved'] = '~r~Enregistre~r~.',
+  ['w_quant'] = '~w~Quantite invalide~r~!',
   ['item_nfound'] = '~r~Objet non-trouvé~w~!',
   ['error'] = '~r~Erreur~w~!',
+  ---Titres
+  ['weap'] = 'Armes',
+  ['quantity'] = 'Quantite',
+  ['trunk'] = 'Botte de voiture',
+  ['sure'] = 'Etes-vous sur de vouloir enregistrer un',
+  ['surer'] = 'Etes-vous sur de vouloir prendre le',
+  ['add_item'] = 'Liste des articles',
+  ['add_weap'] = 'Liste des armes',
  }
