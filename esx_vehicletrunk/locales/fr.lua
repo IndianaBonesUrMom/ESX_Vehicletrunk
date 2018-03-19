b245487080a656d2b95b92f19b14af77b8827208
MrLumi <lumilesff@hotmail.com>
2018-03-19 16:26:11 +0000

    Create fr.lua

---
+++ esx_vehicletrunk/locales/fr.lua
@@ -0,0 +1,8 @@
+Locales['fr'] = {
+  ['add_item'] = 'l'article était ~g~stocké~w~.',
+  ['trunk_full'] = 'Le coffre est ~w~plein~r~',
+  ['saved'] = '~r~Enregistré~r~.',
+  ['w_quant'] = '~w~Quantité invalide~r~!',
+  ['item_nfound'] = '~r~Objet non-trouvé~w~!',
+  ['error'] = '~r~Erreur~w~!',
+ }
