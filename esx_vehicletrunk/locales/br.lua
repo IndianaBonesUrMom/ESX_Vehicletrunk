f3ee7888fef1f21df3dded92ede5bf17acd57f47
MrLumi <lumilesff@hotmail.com>
2018-03-19 16:23:38 +0000

    Create br.lua

---
+++ esx_vehicletrunk/locales/br.lua
@@ -0,0 +1,8 @@
+Locales['br'] = {
+  ['add_item'] = 'O item esta ~g~armazenado~w~.',
+  ['trunk_full'] = 'A bagageira está ~w~cheia~r~',
+  ['saved'] = '~r~Guardado~r~.',
+  ['w_quant'] = '~w~Quantidade invalida~r~!',
+  ['item_nfound'] = '~r~Item nao encontrado~w~!',
+  ['error'] = '~r~Erro~w~!',
+ }
