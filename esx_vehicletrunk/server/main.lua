f409f2211ceef4c3a895600d669dc7e653097265
Arkansas Tom Jones <atte.kuosmanen@edu.savonia.fi>
2018-04-01 22:54:56 +0300

    Update main.lua

--- esx_vehicletrunk/server/main.lua
+++ esx_vehicletrunk/server/main.lua
@@ -30,9 +30,8 @@ AddEventHandler('esx:playerDropped', function(source)
 		dbg("Trunk " .. i .." released")
 		if Config.EnableDupeProtection then
 			MySQL.Async.execute('DELETE FROM vehicle_trunks WHERE plate = @plate', { ['@plate'] = i }, function(r) TrunksInUse[j] = nil dbg("Trunk content deleted") end)
-		else
-			TrunksInUse[i] = nil
 		end
+		TrunksInUse[i] = nil
 		break
 	end
   end
